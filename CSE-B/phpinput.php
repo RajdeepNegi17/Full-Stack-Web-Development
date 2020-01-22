a210363bc5a1c76fb35054086c71d8b9cacc600a
RAJDEEPNEGI17 <rajdeepnegi.rn@gmail.com>
2020-01-22 14:53:14 +0530

    phpinput modified

--- CSE-B/phpinput.php
+++ CSE-B/phpinput.php
@@ -1,5 +1,19 @@
+<?php
+include('config.php');
+?>
+
 <?php 
-	echo 'Welcome'.$_GET['username'];
+if (isset($_POST['btn'])){
+	echo "Dear, ".$_POST['username']."Your Details are".'<br>';
+	echo $_POST['id'].'<br>';
+	echo $_POST['username'].'<br>';
+	echo $_POST['age'].'<br>';
+	echo $_POST['contact'].'<br>';
+	echo $_POST['email'];
+}
+else{
+	echo "WElcome guest";
+}
 ?>
 <!DOCTYPE html>
 <html>
@@ -7,9 +21,13 @@
 	<title>Input Form</title>
 </head>
 <body>
-	<form name="form" action="phpinput.php" method="get">
-		Name: <input type="text" name="username"><br>
-		<input type="submit" name="btn">
+	<form name="form" action="phpinput.php" method="post">
+		ID: <input type="text" name="id"  placeholder="Enter ID"><br>
+		Name: <input type="text" name="username" placeholder="Enter Name"><br>
+		Age: <input type="text" name="age" placeholder="Enter Age"><br>
+		Contact: <input type="text" name="contact" placeholder="Enter Contact"><br>
+		E-mail: <input type="text" name="email" placeholder="Enter email"><br>
+		<input type="submit" name="btn"><br>
 	</form>
 
 </body>
