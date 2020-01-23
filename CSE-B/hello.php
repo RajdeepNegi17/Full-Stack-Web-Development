10da4291ab1719e2f423fd0636d02029f6f3512a
RAJDEEPNEGI17 <rajdeepnegi.rn@gmail.com>
2020-01-23 14:19:44 +0530

    new page details.php

--- CSE-B/hello.php
+++ CSE-B/hello.php
@@ -1,3 +1,14 @@
 <?php
-echo ("<h1>Welcome to Graphic Era!!</h1>");
+$hostName = 'localhost';
+$userName = 'root';
+$password = '';
+$databaseName = 'CSEB';
+
+if(mysqli_connect($hostName,$userName,$password,$databaseName)){
+	echo 'Connection Successful';
+}
+else{
+	echo 'connection_failed!!' + 
+	mysqli_connect_error();
+}
 ?>
--- CSE-B/phpinput.php
+++ CSE-B/phpinput.php
@@ -2,26 +2,13 @@
 include('config.php');
 ?>
 
-<?php 
-if (isset($_POST['btn'])){
-	echo "Dear, ".$_POST['username']."Your Details are".'<br>';
-	echo $_POST['id'].'<br>';
-	echo $_POST['username'].'<br>';
-	echo $_POST['age'].'<br>';
-	echo $_POST['contact'].'<br>';
-	echo $_POST['email'];
-}
-else{
-	echo "WElcome guest";
-}
-?>
 <!DOCTYPE html>
 <html>
 <head>
 	<title>Input Form</title>
 </head>
 <body>
-	<form name="form" action="phpinput.php" method="post">
+	<form name="form" action="details.php" method="post">
 		ID: <input type="text" name="id"  placeholder="Enter ID"><br>
 		Name: <input type="text" name="username" placeholder="Enter Name"><br>
 		Age: <input type="text" name="age" placeholder="Enter Age"><br>
