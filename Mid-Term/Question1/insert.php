4e9be6f150e3f5c6225e4b2a80e73e24c6a17c52
RAJDEEPNEGI17 <rajdeepnegi.rn@gmail.com>
2020-03-20 16:58:18 +0530

    Details Input Page

---
+++ Mid-Term/Question1/insert.php
@@ -0,0 +1,17 @@
+<html>
+	<head>
+	<title>Input Form</title>
+	</head>
+	<body>
+	<form name="form" action="InputRecords.php" method="post">
+		Enrollment: <input type="text" name="Enrollment"  placeholder="Enter ID"><br>
+		Name: <input type="text" name="username" placeholder="Enter Name"><br>
+		Age: <input type="text" name="age" placeholder="Enter Age"><br>
+		Cousre: <input type="text" name="course" placeholder="Enter Course"><br>
+		Branch: <input type="text" name="branch" placeholder="Enter Branch"><br>
+		Contact: <input type="text" name="contact" placeholder="Enter Contact"><br>
+		E-mail: <input type="text" name="email" placeholder="Enter email"><br>
+		<input type="submit" name="btn"><br>
+	</form>
+	</body>
+</html>
